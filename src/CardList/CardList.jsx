e1ba60c58fab81e671c63d1cdc5c5473ded78349
Fathurrobbany <fathurrobbany5@gmail.com>
2024-11-18 20:06:22 +0700

    edit cardlist

--- src/CardList/CardList.jsx
+++ src/CardList/CardList.jsx
@@ -1,23 +1,22 @@
 import PropTypes from "prop-types";
-import Card from "../components/Card/card";
+import Card from "../components/Card/Card";
 
-export default function CardList({product}){
-    return (
+export default function CardList({ product }) {
+  return (
+    <>
+      {product.length > 0 ? (
+        product.map((prod, index) => (
+          <Card product={prod} index={index} key={prod.id ?? index} />
+        ))
+      ) : (
         <>
-        {
-            product.length > 0 ? (
-                product.map((prod, index)  => <Card product={prod} index={index} key=
-                {prod.id ?? index}/> )
-            ) : (
-                <>
-                <p className="text-center py-4 col-span-4">No Product. </p>
-                </>
-            )
-        }
+          <p className="text-center py-4 col-span-4">No Product. </p>
         </>
-    )
+      )}
+    </>
+  );
 }
 
 CardList.propTypes = {
-    product : PropTypes.arrayOf(PropTypes.object).isRequired
-}
+  product: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
